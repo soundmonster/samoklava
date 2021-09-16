5d86ee8a471f6eb263e7e0b34653d76752cc7752
Leo Batyuk <leonid.batyuk@wooga.net>
2021-09-16 21:29:22 +0200

    Wip Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,46 @@
-.phony: ergogen
+.DELETE_ON_ERROR:
 
+container_cmd ?= docker
+container_args ?= -w /board -v $(shell pwd):/board --rm
 
-ergogen:
+output/pcbs/%.kicad_pcb: yamkbd.yaml
 	npm run gen
+
+output/pcbs/%.dsn: output/pcbs/%.kicad_pcb
+	${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py $< $@
+	touch $@
+
+output/routed_pcbs:
+	mkdir -p $@
+
+output/routed_pcbs/%.ses: output/pcbs/%.dsn output/routed_pcbs
+	${container_cmd} run ${container_args} soundmonster/freerouting_cli:v0.1.0 java -jar /opt/freerouting_cli.jar -de $< -do $@
+	touch $@
+
+output/routed_pcbs/%.kicad_pcb: output/routed_pcbs/%.ses output/pcbs/%.kicad_pcb
+	${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py output/pcbs/$*.kicad_pcb $< --output-file $@
+	touch $@
+
+output/routed_pcbs/%-drc/: output/routed_pcbs/%.kicad_pcb
+	${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/run_drc.py  $< $@
+	touch $@
+
+output/routed_pcbs/%-front.png: output/routed_pcbs/%.kicad_pcb
+	${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 pcbdraw --style builtin:set-white-enig.json $< $@
+	touch $@
+
+output/routed_pcbs/%-back.png: output/routed_pcbs/%.kicad_pcb
+	${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 pcbdraw -b --style builtin:set-white-enig.json $< $@
+	touch $@
+
+output/gerbers/%/: output/routed_pcbs/%.kicad_pcb
+	mkdir -p $@
+	${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 kikit fab jlcpcb --no-assembly $< $@
+	touch $@
+
+.PHONY: clean
+clean:
+	rm -rf output
+
+all: output/routed_pcbs/board-front.png output/routed_pcbs/board-back.png output/routed_pcbs/top_plate/gerbers
+
--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# cri_cmd=podman
-cri_cmd=docker
-cri_args="-w /board -v $(pwd):/board --rm"
+# container_cmd=podman
+container_cmd=docker
+container_args="-w /board -v $(pwd):/board --rm"
 
 rm -rf output
 npm run gen
-${cri_cmd} run ${cri_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py output/pcbs/main.kicad_pcb output/pcbs/main.dsn
-${cri_cmd} run ${cri_args} soundmonster/freerouting_cli:v0.1.0 java -jar /opt/freerouting_cli.jar -de output/pcbs/main.dsn -do output/pcbs/main.ses
-${cri_cmd} run ${cri_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py output/pcbs/main.kicad_pcb output/pcbs/main.ses --output-file output/pcbs/main-routed.kicad_pcb
-${cri_cmd} run ${cri_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/run_drc.py output/pcbs/main-routed.kicad_pcb output/pcbs/drc/
+${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/export_dsn.py output/pcbs/main.kicad_pcb output/pcbs/main.dsn
+${container_cmd} run ${container_args} soundmonster/freerouting_cli:v0.1.0 java -jar /opt/freerouting_cli.jar -de output/pcbs/main.dsn -do output/pcbs/main.ses
+${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/import_ses.py output/pcbs/main.kicad_pcb output/pcbs/main.ses --output-file output/pcbs/main-routed.kicad_pcb
+${container_cmd} run ${container_args} soundmonster/kicad-automation-scripts:latest /usr/lib/python2.7/dist-packages/kicad-automation/pcbnew_automation/run_drc.py output/pcbs/main-routed.kicad_pcb output/pcbs/drc/
 # pcbdraw also supports a style file as JSON
-${cri_cmd} run ${cri_args} yaqwsx/kikit:v0.7 pcbdraw --style builtin:set-white-enig.json output/pcbs/main-routed.kicad_pcb images/left.png
-${cri_cmd} run ${cri_args} yaqwsx/kikit:v0.7 pcbdraw -b --style builtin:set-white-enig.json output/pcbs/main-routed.kicad_pcb images/right.png
-${cri_cmd} run ${cri_args} yaqwsx/kikit:v0.7 kikit fab jlcpcb --no-assembly output/pcbs/main-routed.kicad_pcb production/pcb/main
+${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 pcbdraw --style builtin:set-white-enig.json output/pcbs/main-routed.kicad_pcb images/left.png
+${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 pcbdraw -b --style builtin:set-white-enig.json output/pcbs/main-routed.kicad_pcb images/right.png
+${container_cmd} run ${container_args} yaqwsx/kikit:v0.7 kikit fab jlcpcb --no-assembly output/pcbs/main-routed.kicad_pcb production/pcb/main
 
