f30960a32a7e573e3e1e23e69156fffdec0f29e3
Leo B <leo.the.soundmonster@gmail.com>
2023-08-06 20:28:19 +0200

    Fix text footprint

--- footprints/text.js
+++ footprints/text.js
@@ -1,18 +1,22 @@
 module.exports = {
-    params: {
-        layer: 'F.SilkS',
-        text: '',
-        h_size: 1,
-        v_size: 1,
-        thickness: 0.15,
-        justify: null
-    },
-    body: p => {
-        justify = p.param.justify && `(justify ${p.param.justify})` || '';
-        return `
-            (gr_text "${p.param.text}" ${p.at} (layer ${p.param.layer})
-                (effects (font (size ${p.param.h_size} ${p.param.v_size}) (thickness ${p.param.thickness})) ${justify})
-            )
-        `
+  params: {
+    layer: "F.SilkS",
+    text: "",
+    h_size: 1,
+    v_size: 1,
+    thickness: 0.15,
+    justify: "",
+  },
+  body: (p) => {
+    let justify = "";
+    if (p.justify != "") {
+      justify = `(justify ${p.justify})`;
     }
-}
+
+    return `
+            (gr_text "${p.text}" ${p.at} (layer ${p.layer})
+                (effects (font (size ${p.h_size} ${p.v_size}) (thickness ${p.thickness})) ${justify})
+            )
+        `;
+  },
+};
